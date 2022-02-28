ef17fb339a74bfb4403e66ea7bf9762cfa3f6a57
KYRIEZX <xuz@umass.edu>
2022-02-28 18:17:13 -0500

    modified:   src/main/java/com/company/Client/Client.java

--- src/main/java/com/company/Client/Client.java
+++ src/main/java/com/company/Client/Client.java
@@ -1,5 +1,21 @@
 package com.company.Client;
 
-public class Client {
+import com.alibaba.fastjson.JSONObject;
 
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 8088);
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        BufferedReader buf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        boolean flag = true;
+        while(flag){
+            JSONObject json = new JSONObject();
+            json.put("Method", "query");
+            json.put("toyName", "Tux");
+            oos.write(json.toString().getBytes());
+        }
+    }
 }
