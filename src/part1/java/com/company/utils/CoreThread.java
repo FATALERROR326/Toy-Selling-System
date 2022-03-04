390d9634b5f59e37b42a77f0a53614da056092ac
linfeng <1378086613@qq.com>
2022-03-04 15:51:15 -0500

    changes

--- src/part1/java/com/company/utils/CoreThread.java
+++ src/part1/java/com/company/utils/CoreThread.java
@@ -1,15 +1,11 @@
 package com.company.utils;
 
-import com.company.Products.Stock;
-
 public class CoreThread extends Thread{
     private MyBlockingQueue<Runnable> workingQueue;
 
     public CoreThread(MyBlockingQueue<Runnable> workingQueue) {
         this.workingQueue = workingQueue;
     }
-
-
 
     @Override
     public void run(){
