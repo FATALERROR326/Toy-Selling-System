d63b66f5bf5eee0397d124314683e9c612e6510c
linfeng <1378086613@qq.com>
2022-03-03 19:45:40 -0500

    add test part and request entity

--- src/part1/java/com/company/Products/Stock.java
+++ src/part1/java/com/company/Products/Stock.java
@@ -1,8 +1,6 @@
 package com.company.Products;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
